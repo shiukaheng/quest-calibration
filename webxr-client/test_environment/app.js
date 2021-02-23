cb0e4514ddd33827cd3fc9ee45f2042ea315b47f
Heng <31088159+shiukaheng@users.noreply.github.com>
2021-02-23 18:51:37 +0800

    webxr-client added demo

--- webxr-client/test_environment/app.js
+++ webxr-client/test_environment/app.js
@@ -1,8 +1,35 @@
-var dataDisplay = document.createElement("div")
-document.body.appendChild(dataDisplay)
+var dataDisplay = document.getElementById("dataDisplay")
+var addressInput = document.getElementById("address")
+var connectButton = document.getElementById("connectButton")
 
-var client = new QuestCalibrationClient.QuestCalibrationWebXRClient("dummy")
-client.connect()
-client.addCallback((data)=>{
-    dataDisplay.innerText = JSON.stringify(data)
-})
+var client
+
+function connectTo(address) {
+    if (client !== undefined) {
+        client.disconnect()
+        console.log(`Disconnected from old address: ${client.serverUrl}`)
+    }
+    try {
+        console
+        client = new QuestCalibrationClient.QuestCalibrationWebXRClient(address)
+        client.connect().then(()=>{console.log(`Connected to new address: ${client.serverUrl}`)})
+        client.addCallback((data)=>{
+        dataDisplay.innerText = JSON.stringify(data, null, 2)
+        })
+    } catch (error) {
+        if (error instanceof ReferenceError) {
+            dataDisplay.innerText = "QuestCalibrationClient library not in global scope. Perhaps it has not been compiled yet?"
+        } else {
+            throw error
+        }
+    }    
+}
+
+connectButton.onclick = () => {
+    if (address.value === "") {
+        connectTo(address.placeholder)
+    } else {
+        connectTo(address.value)
+    }
+}
+
