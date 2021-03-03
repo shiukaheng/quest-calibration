bb268a60bd80f14aa332a379033276bf03327310
Heng <31088159+shiukaheng@users.noreply.github.com>
2021-03-03 16:20:39 +0800

    added scripts and drafted classes

---
+++ webxr-client/source/BaseClient.ts
@@ -0,0 +1,14 @@
+class ClientMetadataContainer {
+    // split into mandatory and
+}
+
+class BaseClient {
+    constructor() {
+        // BaseClient will have a base metadata object, classes that inherit BaseClient must also provide these metadata keys.
+    }
+    updateMetadata(key, value) {
+
+    }
+    
+
+}
